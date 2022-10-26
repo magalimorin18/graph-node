596e768a8ea5a3ede06745e569bd11c758be20c8
Magali Morin <magali@lukso.io>
2022-10-26 12:05:32 +0200

    Delete graph-node folder
    Generating a graph node locally only requires a docker-compose file and not the whole folder.

--- graph-node/chain/substreams/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=proto");
-    tonic_build::configure()
-        .out_dir("src/protobuf")
-        .compile(&["codec.proto"], &["proto"])
-        .expect("Failed to compile Substreams entity proto(s)");
-}
